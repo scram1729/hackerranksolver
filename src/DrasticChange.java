46fe828b3bfd584c26dbeba2243d0cde71583886
scram1729 <venkatramnath1998@gmail.com>
2021-07-20 14:30:33 +0530

    third commit

--- src/DrasticChange.java
+++ src/DrasticChange.java
@@ -7,7 +7,7 @@ public class DrasticChange {
 		for(int i=str.length()-1;i>=0;i--) {
 			System.out.println(str.charAt(i));
 		}
-		System.out.println("tresspass.....");
+		System.out.println("tresspass....."+"tres..........");
 		
 	}
 
